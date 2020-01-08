6743100c11a87551b0298d41fead39865806ba35
brogan89 <brogan.king@outlook.com>
2020-01-08 19:57:25 +1300

    added icons to sub headers

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
+
+// initialises instagram feed
 var feed = new Instafeed({
     get: 'user',
-    // limit: 12,
+    limit: 12,
     userId: '20401021174',
     accessToken: '20401021174.1677ed0.7822cee968454bbb9b932d169072e20a',
     template: '<div class="instaimg"><a href="{{link}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
 });
 feed.run();
+
+// opens topbar menu when in mobile mode
+function openMenu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
